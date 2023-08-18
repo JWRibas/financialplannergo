eec537bb9071f5a05497f59c004302b77fa08efb
Junior Ribas <133827028+JWRibas@users.noreply.github.com>
2023-08-18 20:42:12 -0300

    "first"

--- adapter/http/main.go
+++ adapter/http/main.go
@@ -3,6 +3,7 @@ package http
 import (
 	"github.com/JWRibas/financialplannergo/adapter/http/actuater"
 	"github.com/JWRibas/financialplannergo/adapter/http/transaction"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log"
 	"net/http"
 )
@@ -12,6 +13,8 @@ func Init() {
 	http.HandleFunc("/transactions/create", transaction.CreatATransaction)
 
 	http.HandleFunc("/health", actuater.Health)
+
+	http.Handle("/metrics", promhttp.Handler())
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
